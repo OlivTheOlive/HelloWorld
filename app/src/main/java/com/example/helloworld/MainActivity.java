6c62ce7521d51ccf5e05382ec3d6712fc8e088ff
OlivTheOlive <111806530+OlivTheOlive@users.noreply.github.com>
2023-05-18 19:32:36 -0400

    did up to part 2 for lab 2

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.BreakIterator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView theText =  findViewById( R.id.theText );
 
+        EditText myedit = findViewById( R.id.theEditText);
+        myedit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String editString = myedit.getText().toString();
+                theText.setText("you clicked the button"+editString);
+            }
+        });
     }
 }
