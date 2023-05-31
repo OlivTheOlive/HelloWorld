f95ae77a5ec2dba8fcd97ab47fcd28dbb8c21f13
OlivTheOlive <111806530+OlivTheOlive@users.noreply.github.com>
2023-05-31 12:53:00 -0400

    lab 3 part 2 done

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
+
+import com.google.android.material.internal.VisibilityAwareImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
+    Switch sw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.flag_view);
+        sw = findViewById(R.id.switch1);
+
+        sw.setOnCheckedChangeListener((btn, isChecked)->{
+            if(isChecked){
+                RotateAnimation rotate = new RotateAnimation(0,360, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF, 0.0f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+
+                imageView.startAnimation(rotate);
+            }
+            else {
+                imageView.clearAnimation();
+            }
+        });
     }
 }
